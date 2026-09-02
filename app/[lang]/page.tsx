82c82c39b5d8213df0ec166a49b3337cdc39eb0f
Martín Beraldi <martinberaldi@MacBook-Pro-de-Martin.local>
2026-09-03 01:26:52 +0200

    Remove Como, ParaRestaurantes, Club, Cifras, RutaMapa sections from home

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,11 +1,6 @@
 import { notFound } from 'next/navigation';
 import Hero from '@/components/sections/Hero';
-import Cifras from '@/components/sections/Cifras';
-import RutaMapa from '@/components/sections/RutaMapa';
 import Bodegas from '@/components/sections/Bodegas';
-import Como from '@/components/sections/Como';
-import ParaRestaurantes from '@/components/sections/ParaRestaurantes';
-import Club from '@/components/sections/Club';
 import Martin from '@/components/sections/Martin';
 import Contacto from '@/components/sections/Contacto';
 import { getDictionary } from '@/lib/dictionaries';
@@ -20,12 +15,7 @@ export default async function Home({ params }: { params: Promise<{ lang: string 
   return (
     <main>
       <Hero lang={l} dict={dict} />
-      <Cifras dict={dict} />
-      <RutaMapa dict={dict} />
       <Bodegas dict={dict} />
-      <Como dict={dict} />
-      <ParaRestaurantes lang={l} dict={dict} />
-      <Club lang={l} dict={dict} />
       <Martin dict={dict} />
       <Contacto dict={dict} />
     </main>
